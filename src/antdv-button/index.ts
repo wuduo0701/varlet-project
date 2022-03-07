6b0ecdb81f68ca1560d417d3637319845bcbdb21
linjiahao7 <linjiahao7@jd.com>
2022-03-07 12:51:41 +0800

    fix: 修改antdv封装的button命名

--- src/antdv-button/index.ts
+++ src/antdv-button/index.ts
@@ -1,10 +1,10 @@
 import type { App } from 'vue'
-import Button from './antdvButton.vue'
+import AntdvButton from './antdvButton.vue'
 
-Button.install = function (app: App) {
+AntdvButton.install = function (app: App) {
   app.component(Button.name, Button)
 }
 
-export const _ButtonComponent = Button
+export const _AntdvButtonComponent = AntdvButton
 
-export default Button
+export default AntdvButton
